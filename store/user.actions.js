2962d00534bf74d782c66d1398407588cc7188c2
Ofek Cohen <ofek074@gmail.com>
2025-11-24 18:20:00 +0200

    Create user.actions.js

--- store/user.actions.js
+++ store/user.actions.js
@@ -1,2 +1,35 @@
 import { userService } from "../services/user.service.js"
 import {SET_USER, store } from "./store.js"
+
+export function login(credentials) {
+
+    return userService.login(credentials)
+        .then(loggedinUser => {
+            store.dispatch({ type: SET_USER, loggedinUser })
+        })
+        .catch(err => {
+            throw err
+        })
+}
+
+export function signup(credentials) {
+
+    return userService.signup(credentials)
+        .then(loggedinUser => {
+            store.dispatch({ type: SET_USER, loggedinUser })
+        })
+        .catch(err => {
+            throw err
+        })
+}
+
+export function logout() {
+
+    return userService.logout()
+        .then(() => {
+            store.dispatch({ type: SET_USER, loggedinUser: null })
+        })
+        .catch(err => {
+            throw err
+        })
+}

f607495d3dea73e788130c8b600f3ff46bcb73dd
Ofek Cohen <ofek074@gmail.com>
2025-11-24 18:06:13 +0200

    Create store.js

---
+++ store/store.js
@@ -0,0 +1,69 @@
+const { createStore } = Redux;
+import { userService } from "../services/user.service.js";
+const initialState = {
+  todos: [],
+  filterBy: {},
+  isLoading: false,
+  loggedinUser: userService.getLoggedinUser(),
+};
+// Todo
+export const SET_TODOS = "SET_TODOS";
+export const REMOVE_TODO = "REMOVE_TODO";
+export const ADD_TODO = "ADD_TODO";
+export const UPDATE_TODO = "UPDATE_TODO";
+export const SET_IS_LOADING = "SET_IS_LOADING";
+
+export const SET_FILTER_BY = "SET_FILTER_BY";
+export const REMOVE_TODO_UNDO = "REMOVE_TODO_UNDO";
+
+// User
+export const SET_USER = "SET_USER";
+
+function appReducer(state = initialState, cmd = {}) {
+  switch (cmd.type) {
+    case SET_TODOS:
+      return {
+        ...state,
+        todos: [...cmd.todos],
+      };
+    case REMOVE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo._id !== cmd.todoId),
+      };
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [cmd.todo, ...state.todos],
+      };
+    case UPDATE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo._id === cmd.todo._id ? cmd.todo : todo
+        ),
+      };
+    case SET_IS_LOADING:
+      return {
+        ...state,
+        isLoading: cmd.isLoading,
+      };
+    case SET_USER:
+      return {
+        ...state,
+        loggedinUser: cmd.loggedinUser,
+      };
+    case SET_FILTER_BY:
+      return {
+       ...state,
+       filterBy: {...cmd.filterBy} 
+      };
+
+    default:
+      return state;
+  }
+}
+
+export const store = createStore(appReducer);
+
+window.gStore = store;
